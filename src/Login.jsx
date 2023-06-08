94fdc04d116aedddf80cabca9c3cb92c71370155
Shivani Jetty <shivanijetty@gmail.com>
2023-06-08 10:58:48 -0400

    catching up

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,20 +3,26 @@ import { useState } from 'react'
 import './App.css'
 
 function Login() {
-  const formData = {}
+  const [users, setUsers] = useState([])
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
 
 
   return(
-    <div className="background">
-      <div className="login-form">
-        <form>
-          <input type="email" value='email' name='email' placeholder="EMAIL" /><br />
-          <input type="password" value='password' name='password' placeholder="PASSWORD" /><br />
-          <div>
-            <input type="submit" />
-            <button onClick={() => { navigate('/home') }}>Sign Up</button>
-          </div>
-        </form>
+    <div>
+      <div className="background">
+        <div className="login-form">
+          <form>
+            <input type="email" value='email' name='email' placeholder="EMAIL" /><br />
+            <input type="password" value='password' name='password' placeholder="PASSWORD" /><br />
+            <div>
+              <input type="submit" />
+              <button onClick={() => { navigate('/home') }}>Sign Up</button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   )
